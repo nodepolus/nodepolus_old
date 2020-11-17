1098b42b8d5ec3111520dd6f19578b1e74d92276
Rose <rose@hall.ly>
2020-11-17 01:22:55 -0500

    Fixed snapTo

--- lib/packets/subpackets/gameDataPackets/rpcPackets/snapTo.ts
+++ lib/packets/subpackets/gameDataPackets/rpcPackets/snapTo.ts
@@ -4,18 +4,24 @@ import Vector2 from "../../../packetElements/vector2";
 
 export interface SnapToPacket {
   Position: Vector2;
+  SequenceID: number
 }
 
 export const SnapTo: PacketHandler<SnapToPacket> = {
   parse(packet: PolusBuffer): SnapToPacket {
     const pos = new Vector2();
     pos.parse(packet);
+    let SequenceID = packet.readU16()
     return {
       Position: pos,
+      SequenceID
     };
   },
 
   serialize(packet: SnapToPacket): PolusBuffer {
-    return packet.Position.serialize();
+    let pb = new PolusBuffer(6)
+    pb.writeBytes(packet.Position.serialize())
+    pb.writeU16(packet.SequenceID)
+    return pb
   },
 };
