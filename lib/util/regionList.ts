db7b60924f9bcb7c8756bba1344914c9832d16ca
Amon Green <drwhorx@gmail.com>
2020-11-26 00:37:46 -0500

    Remove unnecessary PolusBuffer arguments

--- lib/util/regionList.ts
+++ lib/util/regionList.ts
@@ -22,29 +22,29 @@ export class RegionList {
 
   public getBuffer(): PolusBuffer {
     let buffer = new PolusBuffer();
-    buffer.write32(this.selected, false); // selected server
+    buffer.write32(this.selected); // selected server
     buffer.writeString(this.name); // region name
     buffer.writeString(this.address); // region ping ip
-    buffer.write32(this.servers.length, false); // server count
+    buffer.write32(this.servers.length); // server count
     for (let server of this.servers) {
       buffer.writeString(server.name); //server name: usually <region-name>-MASTER-<number>
       buffer.writeBytes(server.address.split(".").map((e) => parseInt(e))); // server ip as a four byte array
-      buffer.write16(server.port, false); // server port
-      buffer.write32(server.failures, false); // number of connection failures
+      buffer.write16(server.port); // server port
+      buffer.write32(server.failures); // number of connection failures
     }
     return buffer;
   }
 
   public fromBuffer(buffer: PolusBuffer) {
-    this.selected = buffer.read32(false);
+    this.selected = buffer.read32();
     this.name = buffer.readString();
     this.address = buffer.readString();
-    this.servers = new Array(buffer.read32(false));
+    this.servers = new Array(buffer.read32());
     for (let i = 0; i < this.servers.length; i++) {
       let name = buffer.readString();
       let address = buffer.readBytes(4).buf.join(".");
-      let port = buffer.read16(false);
-      let failures = buffer.read32(false);
+      let port = buffer.read16();
+      let failures = buffer.read32();
       this.servers[i] = new Region(name, address, port);
       this.servers[i].failures = failures;
     }
