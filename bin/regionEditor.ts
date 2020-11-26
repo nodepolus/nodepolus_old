c45d85dcabb158f48b5f99b5be9076d20537a856
Amon Green <drwhorx@gmail.com>
2020-11-26 01:56:19 -0500

    Add DNS hostname resolver in regionEditor.ts

--- bin/regionEditor.ts
+++ bin/regionEditor.ts
@@ -2,6 +2,9 @@ import os from "os";
 import fs from "fs";
 import { PolusBuffer } from "../lib/util/polusBuffer";
 import { RegionList } from "../lib/util/regionList";
+import dns from "dns";
+import util from "util";
+const lookup = util.promisify(dns.lookup);
 
 let regionFile: string;
 
@@ -23,13 +26,22 @@ function writeBuffer(regionList: PolusBuffer) {
   fs.writeFileSync(regionFile, regionList.buf, "hex");
 }
 
-if (process.argv[2] == "set-region") {
-  let list = new RegionList(process.argv[3], process.argv[4]);
-  writeBuffer(list.getBuffer());
-} else if (process.argv[2] == "add-server") {
-  let list = RegionList.fromBuffer(
-    new PolusBuffer(fs.readFileSync(regionFile))
-  );
-  list.addServer(process.argv[3], process.argv[4], parseInt(process.argv[5]));
-  writeBuffer(list.getBuffer());
-}
+(async () => {
+  if (process.argv[2] == "set-region") {
+    let list = new RegionList(
+      process.argv[3],
+      (await lookup(process.argv[4])).address
+    );
+    writeBuffer(list.getBuffer());
+  } else if (process.argv[2] == "add-server") {
+    let list = RegionList.fromBuffer(
+      new PolusBuffer(fs.readFileSync(regionFile))
+    );
+    list.addServer(
+      process.argv[3],
+      (await lookup(process.argv[4])).address,
+      parseInt(process.argv[5])
+    );
+    writeBuffer(list.getBuffer());
+  }
+})();
