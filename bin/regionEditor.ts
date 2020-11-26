bcc0944d82786b7f4ed4df3e76e7090b205e4d6b
Amon Green <drwhorx@gmail.com>
2020-11-26 00:17:13 -0500

    Create regionEditor CLI

--- bin/regionEditor.ts
+++ bin/regionEditor.ts
@@ -1,27 +1,34 @@
 import os from "os";
 import fs from "fs";
 import { PolusBuffer } from "../lib/util/polusBuffer";
+import { RegionList } from "../lib/util/regionList";
 
-export class regionEditor {
-  regionFile: string;
+let regionFile: string;
 
-  constructor(regionFile: string) {
-    if (!regionFile) {
-      let home = os.homedir();
-      if (
-        fs.existsSync(
-          home + "/AppData/LocalLow/Innersloth/Among Us/regioninfo.dat"
-        )
-      )
-        this.regionFile =
-          home + "/AppData/LocalLow/Innersloth/Among Us/regioninfo.dat";
-      else throw new Error("regioninfo.dat not found!");
-    } else {
-      this.regionFile = regionFile;
-    }
+if (process.argv[2] == "set-region" && !!process.argv[5]) {
+  regionFile = process.argv[5];
+} else if (process.argv[2] == "add-server" && !!process.argv[6]) {
+  regionFile = process.argv[6];
+} else {
+  let home = os.homedir();
+  let path = home + "/AppData/LocalLow/Innersloth/Among Us/regioninfo.dat";
+  if (fs.existsSync(path)) {
+    regionFile = path;
+  } else {
+    throw new Error("regioninfo.dat not found!");
   }
+}
 
-  public writeBuffer(regionList: PolusBuffer) {
-    fs.writeFileSync(this.regionFile, regionList.buf, "hex");
-  }
+function writeBuffer(regionList: PolusBuffer) {
+  fs.writeFileSync(regionFile, regionList.buf, "hex");
+}
+
+if (process.argv[2] == "set-region") {
+  let list = new RegionList(process.argv[3], process.argv[4]);
+  writeBuffer(list.getBuffer());
+} else if (process.argv[2] == "add-server") {
+  let list = new RegionList();
+  list.fromBuffer(new PolusBuffer(fs.readFileSync(regionFile)));
+  list.addServer(process.argv[3], process.argv[4], parseInt(process.argv[5]));
+  writeBuffer(list.getBuffer());
 }
